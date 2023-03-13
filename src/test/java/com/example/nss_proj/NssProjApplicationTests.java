4d3392e7bb2b836228b0a64c83f894465a0703ff
Breadlyy <kirilllozanov012@gmail.com>
2023-03-13 01:54:59 +0100

    BASE

---
+++ src/test/java/com/example/nss_proj/NssProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nss_proj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NssProjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
