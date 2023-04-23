df0f4a5103ce7b1dcebe26604057079ccda6df80
Breadlyy <kirilllozanov012@gmail.com>
2023-04-23 22:49:16 +0200

    added thymeleaf

--- src/main/java/com/example/nss_proj/controller/MainController.java
+++ src/main/java/com/example/nss_proj/controller/MainController.java
@@ -1,13 +1,15 @@
 package com.example.nss_proj.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
-    @GetMapping("/")
-    public String index()
+    @GetMapping("/index")
+    public String index(Model model)
     {
+        model.addAttribute("theDate", new java.util.Date());
         return "index";
     }
 }
