191cb886dc6a7a33b3b0ec1b7d81c03de35cb4e5
Breadlyy <kirilllozanov012@gmail.com>
2023-05-04 15:25:43 +0200

    ...

---
+++ src/main/java/com/nss/account/AccountApplication.java
@@ -0,0 +1,9 @@
+package com.nss.account;
+
+import org.springframework.boot.SpringApplication;
+
+public class AccountApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AccountApplication.class, args);
+    }
+}
